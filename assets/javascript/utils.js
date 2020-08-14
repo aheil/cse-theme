ca65fc511859e66c2a547eb057796e922179efef
Claudia <claudia@Claudias-MacBook-Pro.local>
2020-08-14 20:50:59 +0200

    multipage highlighting fix

--- assets/javascript/utils.js
+++ assets/javascript/utils.js
@@ -1,3 +1,8 @@
+function getStorageKey() {
+    let key = window.location.pathname + "-text-highlights";
+    return key;
+}
+
 function addEventListenerToHighlights() {
     let highlights = document.querySelectorAll(".text-highlighted");
     highlights.forEach(function (highlight) {
@@ -7,9 +12,10 @@ function addEventListenerToHighlights() {
                 let textNode = document.createTextNode(this.innerHTML);
                 this.parentNode.insertBefore(textNode, this);
                 this.parentNode.removeChild(this);//<span>x</span>
-                e.preventDefault();
+                e.preventDefault();//once we click a highlight that is a link we don't want the link to open
 
-                localStorage.setItem("text-highlights", hltr.serializeHighlights());
+                //which page are we on?
+                localStorage.setItem(getStorageKey(), hltr.serializeHighlights());
 
         });
     });
@@ -19,13 +25,13 @@ let hltr = new TextHighlighter(document.querySelector('section'), {
     color: "#ccff00",
     onAfterHighlight: function (range) {
         //dump highlights out to local storage again
-        localStorage.setItem("text-highlights", hltr.serializeHighlights());
+        localStorage.setItem(getStorageKey(), hltr.serializeHighlights());
         addEventListenerToHighlights();
     }
 });
 
 // if highlights are stored locally, pull them up
-let storedHighlights = localStorage.getItem("text-highlights");
+let storedHighlights = localStorage.getItem(getStorageKey());
 if (storedHighlights.length != null)
     hltr.deserializeHighlights(storedHighlights);
 addEventListenerToHighlights();
