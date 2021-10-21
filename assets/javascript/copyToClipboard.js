7d7b6844455461d186d9ca65b81ec8ea9d686ef0
Claudia Hauff <claudia.hauff@gmail.com>
2021-10-22 01:11:34 +0200

    selective copy button

--- assets/javascript/copyToClipboard.js
+++ assets/javascript/copyToClipboard.js
@@ -1,7 +1,22 @@
 const codeSnippets = document.querySelectorAll("div.highlight");
 console.log(`%c ${codeSnippets.length} code snippet(s) found.`, styles["info"]);
 
+const languagesToCopy = new Set();
+languagesToCopy.add("language-javascript");
+languagesToCopy.add("language-css");
+languagesToCopy.add("language-html");
+languagesToCopy.add("language-http");
+
 codeSnippets.forEach((codeSnippet) => {
+  let allowCopy = false;
+  codeSnippet.parentElement.classList.forEach((className) => {
+    if (languagesToCopy.has(className)) {
+      allowCopy = true;
+    }
+  });
+
+  if (!allowCopy) return;
+
   const copyButton = document.createElement("button");
   copyButton.classList.add("copy-button");
   copyButton.innerHTML = "Copy";
